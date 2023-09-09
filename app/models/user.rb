378ad64af3b2e0ad9599be5d406da9f801c77bdf
Jon Evans <joncodes+github@gmail.com>
2023-09-09 13:20:35 -0600

    Add session/registration links, allow name for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
+
+  validates :name, :email, presence: true
 end
