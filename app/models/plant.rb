c02c4389eb36c0e6e09babf010e8bfc393fc175e
Jon Evans <joncodes+github@gmail.com>
2023-10-22 13:27:12 -0600

    Update views to use new image model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,10 +2,7 @@ class Plant < ApplicationRecord
   belongs_to :user
   has_many :measurements, dependent: :destroy
 
-  has_many_attached :images do |attachable|
-    attachable.variant :thumb, resize_to_limit: [100, 100]
-    attachable.variant :medium, resize_to_limit: [500, 500]
-  end
+  has_many :images, as: :imageable
 
   validates :nickname, presence: true
 end
