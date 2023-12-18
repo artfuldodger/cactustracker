aef97df2f869ff1705e85d1ad0ab85c8dcb1b8bd
Jon Evans <joncodes+github@gmail.com>
2023-12-17 19:16:50 -0700

    Link to edit your profile

--- app/controllers/users/plants_controller.rb
+++ app/controllers/users/plants_controller.rb
@@ -1,6 +1,6 @@
 class Users::PlantsController < ApplicationController
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find_by(slug: params[:user_id])
     @plant = @user.plants.find(params[:id])
   end
 end
