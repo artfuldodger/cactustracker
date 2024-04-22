4427b669b1fe97c452f846b85e93baaf435859ce
Anna Dolan <anna.dolan@gmail.com>
2024-04-22 15:40:07 -0600

    Style comment form

--- app/controllers/users/plants_controller.rb
+++ app/controllers/users/plants_controller.rb
@@ -2,6 +2,6 @@ class Users::PlantsController < ApplicationController
   def show
     @user = User.find_by(slug: params[:user_id])
     @plant = @user.plants.find(params[:id])
-    @pagy, @comments = pagy(@plant.comments, items: 10)
+    @pagy, @comments = pagy(@plant.comments, items: 8, fragment:'#comments')
   end
 end
