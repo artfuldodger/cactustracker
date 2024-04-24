da5ebb889fe158a98bd68472cf413360d8c1cc43
Anna Dolan <anna.dolan@gmail.com>
2024-04-24 12:19:17 -0600

    Add pagination and style comments (#15)
    * Add pagination and style comments
    
    * Import variables
    
    * Style comment form
    
    * Import variables more
    
    * Show fewer comments

--- app/controllers/users/plants_controller.rb
+++ app/controllers/users/plants_controller.rb
@@ -2,5 +2,6 @@ class Users::PlantsController < ApplicationController
   def show
     @user = User.find_by(slug: params[:user_id])
     @plant = @user.plants.find(params[:id])
+    @pagy, @comments = pagy(@plant.comments, items: 5, fragment:'#comments')
   end
 end
